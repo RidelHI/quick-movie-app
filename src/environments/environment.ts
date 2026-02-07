b2e35f44419cebf64ffe6a2f55300369d537605e
Ridel Hernández <ridelhi89@gmail.com>
2026-02-07 09:42:00 -0500

    chore: setup ci cd with github actions and pages

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@ export const environment = {
   tmdb: {
     apiBaseUrlV3: 'https://api.themoviedb.org/3',
     apiBaseUrlV4: 'https://api.themoviedb.org/4',
-    readAccessToken: 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4NzllNTRlOWNhYzJjNzQzMzEyN2I5MTMyNjE0MzY3OSIsIm5iZiI6MTc3MDEzNDgyMi43NDUsInN1YiI6IjY5ODIxZDI2NmUzN2NhYzJmNTQxOTNiOCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.ES3AMSxpg9FB02nndF2LxjDn3LoHKnpXHz7RODBi7s8',
+    readAccessToken: 'YOUR_TMDB_READ_ACCESS_TOKEN',
     redirectUri: 'http://localhost:4200/auth/callback',
   },
 };
